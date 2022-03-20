693466a64cab13dc24e15d4da072e9f87b590d9a
Ninh Van <nhvan@go.olemiss.edu>
2022-03-20 00:28:45 -0500

    Add OAuth API Key

--- Frontend/src/containers/Login.js
+++ Frontend/src/containers/Login.js
@@ -3,30 +3,30 @@ import GoogleLogin from 'react-google-login';
 import { FcGoogle } from 'react-icons/fc';
 import './login.css'
 export const Login = () => {
-    const responseGoogle= (response)=> {
-        console.log("Response Google")
+    const responseGoogle = (response) => {
+        console.log(process.env.REACT_APP_GOOGLE_API_TOKEN)
     }
-  return (
-    <div className="container">
-    <div className='icon'>Survey Today Icon</div>
-    <GoogleLogin
-              clientId={`${process.env.REACT_APP_GOOGLE_API_TOKEN}`}
-              render={(renderProps) => (
-                <button
-                  type="button"
-                  className="google-button"
-                  onClick={renderProps.onClick}
-                  disabled={renderProps.disabled}
-                >
-                  <FcGoogle className="google-icon" /> 
-                  <p className='button-text'>Sign in with google</p>
-                </button>
-              )}
-              onSuccess={responseGoogle}
-              onFailure={responseGoogle}
-              cookiePolicy="single_host_origin"
+    return (
+        <div className="container">
+            <div className='icon'>Survey Today Icon</div>
+            <GoogleLogin
+                clientId={process.env.REACT_APP_GOOGLE_API_TOKEN}
+                render={(renderProps) => (
+                    <button
+                        type="button"
+                        className="google-button"
+                        onClick={renderProps.onClick}
+                        disabled={renderProps.disabled}
+                    >
+                        <FcGoogle className="google-icon" />
+                        <p className='button-text'>Sign in with google</p>
+                    </button>
+                )}
+                onSuccess={responseGoogle}
+                onFailure={responseGoogle}
+                cookiePolicy="single_host_origin"
             />
-    </div>
-  )
+        </div>
+    )
 }
 export default Login;
