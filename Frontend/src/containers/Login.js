22a43cf2d728c1822df51f117c8012af440babc7
Mason Glenn <mdglenn@go.olemiss.edu>
2022-03-23 13:35:16 -0500

    Added surveytoday logo to login screen

--- Frontend/src/containers/Login.js
+++ Frontend/src/containers/Login.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import GoogleLogin from 'react-google-login';
 import { FcGoogle } from 'react-icons/fc';
+import logo from '../logo.png';
 import './login.css'
 export const Login = () => {
     const responseGoogle = (response) => {
@@ -8,7 +9,9 @@ export const Login = () => {
     }
     return (
         <div className="container">
-            <div className='icon'>Survey Today Icon</div>
+            <div className='icon'>
+                <img src={logo} alt="Name" width="250" height="250"/>
+            </div>
             <GoogleLogin
                 clientId={process.env.REACT_APP_GOOGLE_API_TOKEN}
                 render={(renderProps) => (
@@ -19,7 +22,7 @@ export const Login = () => {
                         disabled={renderProps.disabled}
                     >
                         <FcGoogle className="google-icon" />
-                        <p className='button-text'>Sign in with google</p>
+                        <p className='button-text'>Sign in with Google</p>
                     </button>
                 )}
                 onSuccess={responseGoogle}
